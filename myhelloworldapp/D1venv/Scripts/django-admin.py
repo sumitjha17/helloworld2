a6d476e92e30d6cea45624edb35215aedc859fab
sumitjha17 <sumitjha2011@ymail.com>
2020-03-28 16:38:02 +0530

    Sumit First Commit

---
+++ myhelloworldapp/D1venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\sumit\desktop\fullthrottle\projects\myhelloworldapp\d1venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
